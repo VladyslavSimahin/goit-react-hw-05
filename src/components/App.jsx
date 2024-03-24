30423ecc8b0fa056a277ab473f2a7958e8eaf366
VladyslavSimahin <vladyslav.simahin@gmail.com>
2024-03-24 17:34:00 -0600

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,4 @@
-// import { FriendList } from "./FriendList";
-// import friends from "./friends.json";
-// import { Payment } from "./Payment/Payment";
-// import prices from "./paymentprice.json";
-
-import { Header } from "./header/Header";
-import { Feedback } from "./feedback/Feedback";
-import { Options } from "./options/Options";
-import { useState, useEffect } from "react";
-
-// const allScore = () => {
-//   const savedScores = window.localStorage.getItem("myScore");
-//   if (savedScores !== null) {
-//     return JSON.parse(savedScores);
-//   }
-//   return {
-//     good: 0,
-//     neutral: 0,
-//     bad,
-//   };
-// };
-export const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    bad: 0,
-    neutral: 0,
-  });
-  const updateFeedback = (feedbackType) => {
-    setFeedback({
-      ...feedback,
-      [feedbackType]: feedback[feedbackType] + 1,
-    });
-  };
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const handelReset = () => {
-    setFeedback({
-      good: 0,
-      bad: 0,
-      neutral: 0,
-    });
-  };
-
-  const positive = Math.round(
-    ((feedback.good + feedback.neutral) / totalFeedback) * 100
-  );
-  // const savedFeedback = localStorage.getItem("feedbacks");
-  // if (savedFeedback) {
-  //   return JSON.parse(savedFeedback);
-  // }
-  // useEffect(() => {
-  //   localStorage.setItem("qFeedback", JSON.stringify(feedback));
-  // }, [feedback]);
-
+export default function App() {
   return (
     <>
       <Header />
@@ -70,5 +18,4 @@ export const App = () => {
       />
     </>
   );
-};
-export default App;
+}
